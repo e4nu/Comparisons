39adedb965ef49a0b8bffdc5a69687362e072a14
jtenavidal <jtenavidal@tauex.tau.ac.il>
2025-07-14 13:26:30 -0500

    adding path instead of file so we can add multiple hempc files

--- src/comparison_1p1pi.cxx
+++ src/comparison_1p1pi.cxx
@@ -16,6 +16,7 @@
 #include "TFile.h"
 #include <TH1D.h>
 #include "Utils.h"
+#include <filesystem>
 
 /////////////////////////////////////////////////////////////////////////////////////
 // comparison_1p1pi.cxx                                                            //
@@ -26,16 +27,21 @@
 int main(int argc, char* argv[]) {
 
   //std::string input_hepmc3_file = "/exp/genie/app/jtena/Comparisons/src/1161/output_electron_C12_1161_351.root";
-  std::string input_hepmc3_file = "/pnfs/genie/persistent/users/jtenavid/e4nu_files/NuHEPMC/Carbon/4453MeV/master-routine_validation_01-eScattering/e_on_1000060120_4453MeV_0.hepmc3";
-///exp/genie/app/jtena/GENIE/Generator/src/scripts/production/python/Generator/e_on_1000060120_4453MeV_0.hepmc3";
+  std::vector<std::string> input_hepmc3_files ={ "/pnfs/genie/persistent/users/jtenavid/e4nu_files/NuHEPMC/Carbon/4453MeV/master-routine_validation_01-eScattering/e_on_1000060120_4453MeV_0.hepmc3"};
+  ///exp/genie/app/jtena/GENIE/Generator/src/scripts/production/python/Generator/e_on_1000060120_4453MeV_0.hepmc3";
   //
   std::string topology = "1p1pim", output_name = "comparison_1p1pi", model_name = "MC_Name" ;
   
   // process options
   if( argc > 1 ) { // configure rest of analysis
-    if( ExistArg("input-hepmc3-file",argc,argv)) {
-      input_hepmc3_file = GetArg("input-hepmc3-file",argc,argv);
-    } //else { std::cout << " --input-hepmc3-file is not defined "; return 0 ;}
+    if( ExistArg("input-hepmc3-directory",argc,argv)) {
+      input_hepmc3_files = {};
+      for (const auto& entry : std::filesystem::directory_iterator(GetArg("input-hepmc3-directory",argc,argv))) {
+        if (entry.is_regular_file() && entry.path().extension() == ".hepmc3") {
+	  input_hepmc3_files.push_back(entry.path().string());  // full path
+        }
+      }
+    } 
     if( ExistArg("output-file",argc,argv)) {
       output_name = GetArg("output-file",argc,argv);
     }
@@ -47,17 +53,17 @@ int main(int argc, char* argv[]) {
     }
   }
 
-  auto rdr = HepMC3::deduce_reader(input_hepmc3_file);
+  auto rdr = HepMC3::deduce_reader(input_hepmc3_files[0]);
   // auto rdr = HepMC3::Reader(input_hepmc3_file);
   if (!rdr) {
-    std::cout << "Failed to instantiate HepMC3::Reader from " << input_hepmc3_file << std::endl;
+    std::cout << "Failed to instantiate HepMC3::Reader from " << input_hepmc3_files[0] << std::endl;
     return 1;
   }
 
   HepMC3::GenEvent evt;
   rdr->read_event(evt);
   if (rdr->failed()) {
-    std::cout << "Failed to read first event from " << input_hepmc3_file << "."
+    std::cout << "Failed to read first event from " << input_hepmc3_files[0] << "."
 	      << std::endl;
     return 1;
   }
@@ -85,140 +91,141 @@ int main(int argc, char* argv[]) {
   auto out_gen_run_info = std::make_shared<HepMC3::GenRunInfo>(*in_gen_run_info);
 
   // re-open the file so that you start at the beginning
-  rdr = HepMC3::deduce_reader(input_hepmc3_file);
-  if (!rdr) {
-    std::cout << "Failed to instantiate HepMC3::Reader from " << input_hepmc3_file << std::endl;
-    return 1;
-  }
-
   size_t nprocessed = 0;
-
-  while (true) { // loop while there are events
-    rdr->read_event(evt);
-    if (rdr->failed()) {
-      break;
-    }
-    ++nprocessed;
-    // Set units to GeV
-    evt.set_units(HepMC3::Units::GEV, HepMC3::Units::MM);
-
-    // Get Run Info:
-    auto beampt = NuHepMC::Event::GetBeamParticle(evt);
-    auto tgtpt = NuHepMC::Event::GetTargetParticle(evt);
-    auto primary_vtx = Event::GetPrimaryVertex(evt);
-    auto process_id = ER3::ReadProcessID(evt);
-    auto proc_ids = GR4::ReadProcessIdDefinitions(in_gen_run_info);
-    if (!beampt || !tgtpt) {  // this event didn't have a beam particle or target, its an odd one
-      continue;
+  for( unsigned int id = 0 ; id < input_hepmc3_files.size() ; ++id ) { 
+    rdr = HepMC3::deduce_reader(input_hepmc3_files[id]);
+    if (!rdr) {
+      std::cout << "Failed to instantiate HepMC3::Reader from " << input_hepmc3_files[id] << std::endl;
+      return 1;
     }
 
-    // Read in-electron properties:
-    auto BeamPdg = beampt->pid();
-    auto Ev = beampt->momentum().e();
-    auto Pxv = beampt->momentum().px();
-    auto Pyv = beampt->momentum().py();
-    auto Pzv = beampt->momentum().pz();
-    if( BeamPdg != 11 ) { std::cout << "ERROR: Beam is not e-"<<std::endl; break; }
-
-    // Read out-electron :
-    auto primary_leptons = NuHepMC::Event::GetParticles_All(evt, NuHepMC::ParticleStatus::UndecayedPhysical, {beampt->pid()} );
-    auto fslep = primary_leptons.back();  
-
-    // Apply cut on Q2
-    auto q = fslep->momentum() - beampt->momentum();
-    auto Q2 = -q.m2();
-    if ( Q2 < GetQ2Cut( Ev ) ) continue ; 
-
-    // Smear particles according to detector Resolution
-    HepMC3::GenParticlePtr fslep_reco = SmearParticles( fslep, Ev ) ;
-    auto FSPrimLept = fslep_reco->pid();
-    auto El = fslep_reco->momentum().e();
-    auto Pxl = fslep_reco->momentum().px();
-    auto Pyl = fslep_reco->momentum().py();
-    auto Pzl = fslep_reco->momentum().pz();
-    auto Pl = fslep_reco->momentum().p3mod();
+    while (true) { // loop while there are events
+      rdr->read_event(evt);
+      if (rdr->failed()) {
+	break;
+      }
+      ++nprocessed;
+      // Set units to GeV
+      evt.set_units(HepMC3::Units::GEV, HepMC3::Units::MM);
     
-    // Apply momentum and angle cuts for electron
-    if( fslep->momentum().theta() * 180 / TMath::Pi() < GetParticleMinTheta( FSPrimLept, Pl, Ev ) ) continue ;
-    if( Pl < GetMinMomentumCut( FSPrimLept, Ev ) ) continue ; 
+      // Get Run Info:
+      auto beampt = NuHepMC::Event::GetBeamParticle(evt);
+      auto tgtpt = NuHepMC::Event::GetTargetParticle(evt);
+      auto primary_vtx = Event::GetPrimaryVertex(evt);
+      auto process_id = ER3::ReadProcessID(evt);
+      auto proc_ids = GR4::ReadProcessIdDefinitions(in_gen_run_info);
+      if (!beampt || !tgtpt) {  // this event didn't have a beam particle or target, its an odd one
+	continue;
+      }
 
-    // Read output particles: 
-    double true_p = 0, true_pip = 0, true_pim = 0, true_gamma = 0, true_n = 0;
-    double reco_p = 0, reco_pip = 0, reco_pim = 0, reco_gamma = 0, reco_n = 0;
-    std::vector<HepMC3::ConstGenParticlePtr> hadrons = NuHepMC::Event::GetParticles_AllRealFinalState(evt,{});
-    std::vector<HepMC3::ConstGenParticlePtr> hadrons_reco;
-    HepMC3::ConstGenParticlePtr pion;
-    HepMC3::ConstGenParticlePtr proton;
-    for( unsigned int i = 0 ; i < hadrons.size() ; ++i ) { 
-      double particle_pdg  = hadrons[i]->pid() ;
-      double particle_e    = hadrons[i]->momentum().e();
-      double particle_px   = hadrons[i]->momentum().px();
-      double particle_py   = hadrons[i]->momentum().py();
-      double particle_pz   = hadrons[i]->momentum().pz();
-      double particle_mod  = hadrons[i]->momentum().p3mod();
+      // Read in-electron properties:
+      auto BeamPdg = beampt->pid();
+      auto Ev = beampt->momentum().e();
+      auto Pxv = beampt->momentum().px();
+      auto Pyv = beampt->momentum().py();
+      auto Pzv = beampt->momentum().pz();
+      if( BeamPdg != 11 ) { std::cout << "ERROR: Beam is not e-"<<std::endl; break; }
 
-      if( particle_pdg == 2212 ) ++true_p ;
-      else if ( particle_pdg == 2112 ) ++true_n ;
-      else if ( particle_pdg == -211 ) ++true_pim ;
-      else if ( particle_pdg == 211 ) ++true_pip;
-      else if ( particle_pdg == 22 ) ++true_gamma;
+      // Read out-electron :
+      auto primary_leptons = NuHepMC::Event::GetParticles_All(evt, NuHepMC::ParticleStatus::UndecayedPhysical, {beampt->pid()} );
+      auto fslep = primary_leptons.back();  
+
+      // Apply cut on Q2
+      auto q = fslep->momentum() - beampt->momentum();
+      auto Q2 = -q.m2();
+      if ( Q2 < GetQ2Cut( Ev ) ) continue ; 
+
+      // Smear particles according to detector Resolution
+      HepMC3::GenParticlePtr fslep_reco = SmearParticles( fslep, Ev ) ;
+      auto FSPrimLept = fslep_reco->pid();
+      auto El = fslep_reco->momentum().e();
+      auto Pxl = fslep_reco->momentum().px();
+      auto Pyl = fslep_reco->momentum().py();
+      auto Pzl = fslep_reco->momentum().pz();
+      auto Pl = fslep_reco->momentum().p3mod();
+    
+      // Apply momentum and angle cuts for electron
+      if( fslep->momentum().theta() * 180 / TMath::Pi() < GetParticleMinTheta( FSPrimLept, Pl, Ev ) ) continue ;
+      if( Pl < GetMinMomentumCut( FSPrimLept, Ev ) ) continue ; 
+
+      // Read output particles: 
+      double true_p = 0, true_pip = 0, true_pim = 0, true_gamma = 0, true_n = 0;
+      double reco_p = 0, reco_pip = 0, reco_pim = 0, reco_gamma = 0, reco_n = 0;
+      std::vector<HepMC3::ConstGenParticlePtr> hadrons = NuHepMC::Event::GetParticles_AllRealFinalState(evt,{});
+      std::vector<HepMC3::ConstGenParticlePtr> hadrons_reco;
+      HepMC3::ConstGenParticlePtr pion;
+      HepMC3::ConstGenParticlePtr proton;
+      for( unsigned int i = 0 ; i < hadrons.size() ; ++i ) { 
+	double particle_pdg  = hadrons[i]->pid() ;
+	double particle_e    = hadrons[i]->momentum().e();
+	double particle_px   = hadrons[i]->momentum().px();
+	double particle_py   = hadrons[i]->momentum().py();
+	double particle_pz   = hadrons[i]->momentum().pz();
+	double particle_mod  = hadrons[i]->momentum().p3mod();
+
+	if( particle_pdg == 2212 ) ++true_p ;
+	else if ( particle_pdg == 2112 ) ++true_n ;
+	else if ( particle_pdg == -211 ) ++true_pim ;
+	else if ( particle_pdg == 211 ) ++true_pip;
+	else if ( particle_pdg == 22 ) ++true_gamma;
       
-      // Smear Hadrons 
-      HepMC3::GenParticlePtr hadron_smeared = SmearParticles( hadrons[i], Ev ) ;
+	// Smear Hadrons 
+	HepMC3::GenParticlePtr hadron_smeared = SmearParticles( hadrons[i], Ev ) ;
 
-      // Apply momentum and angle cuts for hadrons 
-      if( hadron_smeared->momentum().theta() * TMath::RadToDeg() < GetParticleMinTheta( particle_pdg, hadron_smeared->momentum().p3mod(), Ev ) ) continue;
-      if( hadron_smeared->momentum().theta() * TMath::RadToDeg() > 140 ) continue;
-      if( hadron_smeared->momentum().p3mod() < GetMinMomentumCut( particle_pdg, Ev ) ) continue ;
-      if( particle_pdg == 22 && !ApplyPhotRadCut(beampt->momentum(),hadron_smeared->momentum())) continue ;
+	// Apply momentum and angle cuts for hadrons 
+	if( hadron_smeared->momentum().theta() * TMath::RadToDeg() < GetParticleMinTheta( particle_pdg, hadron_smeared->momentum().p3mod(), Ev ) ) continue;
+	if( hadron_smeared->momentum().theta() * TMath::RadToDeg() > 140 ) continue;
+	if( hadron_smeared->momentum().p3mod() < GetMinMomentumCut( particle_pdg, Ev ) ) continue ;
+	if( particle_pdg == 22 && !ApplyPhotRadCut(beampt->momentum(),hadron_smeared->momentum())) continue ;
 
-      // Count detected hadrons 
-      if( particle_pdg == 2212 ) {
-	++reco_p ;
-	proton = hadrons[i];
-      }
-      else if ( particle_pdg == 2112 ) ++reco_n ;
-      else if ( particle_pdg == -211 ) {
-	++reco_pim ;
-	pion = hadrons[i];
-      }
-      else if ( particle_pdg == 211 ) { 
-	++reco_pip;
-	pion = hadrons[i];
-      }
-      else if ( particle_pdg == 22 ) ++reco_gamma;
+	// Count detected hadrons 
+	if( particle_pdg == 2212 ) {
+	  ++reco_p ;
+	  proton = hadrons[i];
+	}
+	else if ( particle_pdg == 2112 ) ++reco_n ;
+	else if ( particle_pdg == -211 ) {
+	  ++reco_pim ;
+	  pion = hadrons[i];
+	}
+	else if ( particle_pdg == 211 ) { 
+	  ++reco_pip;
+	  pion = hadrons[i];
+	}
+	else if ( particle_pdg == 22 ) ++reco_gamma;
       
-      // Save reconstructed hadrons in new vector
-      if( particle_pdg == 2212 || TMath::Abs(particle_pdg) == 211 ) hadrons_reco.push_back(hadron_smeared);
+	// Save reconstructed hadrons in new vector
+	if( particle_pdg == 2212 || TMath::Abs(particle_pdg) == 211 ) hadrons_reco.push_back(hadron_smeared);
  
-    }
+      }
 
-    // Select 1p1pim (- or +) events
-    if ( reco_p != 1 ) continue ; 
-    if ( topology == "1p1pim" ) { 
-      if ( reco_pim != 1 ) continue ; 
-      if ( reco_pip != 0 ) continue ;
-    } else if ( topology == "1p1pip" ) { 
-      if ( reco_pim != 0 ) continue ; 
-      if ( reco_pip != 1 ) continue ;
-    }
-    if ( reco_gamma != 0 ) continue ;
+      // Select 1p1pim (- or +) events
+      if ( reco_p != 1 ) continue ; 
+      if ( topology == "1p1pim" ) { 
+	if ( reco_pim != 1 ) continue ; 
+	if ( reco_pip != 0 ) continue ;
+      } else if ( topology == "1p1pip" ) { 
+	if ( reco_pim != 0 ) continue ; 
+	if ( reco_pip != 1 ) continue ;
+      }
+      if ( reco_gamma != 0 ) continue ;
     
-    // Store histogram with same binning as data. For now is only MC comparison 
-    histograms["Efl"]->Fill(El);
-    histograms["pfl"]->Fill(Pl);
-    histograms["pfl_theta"]->Fill(fslep_reco->momentum().theta()*180/TMath::Pi());
-    histograms["proton_mom"]->Fill(proton->momentum().p3mod());
-    histograms["proton_theta"]->Fill(proton->momentum().theta()*180/TMath::Pi());
-    histograms["pim_mom"]->Fill(pion->momentum().p3mod());
-    histograms["pim_theta"]->Fill(pion->momentum().theta()*180/TMath::Pi());
-    histograms["ECal"]->Fill(GetECal( fslep_reco, hadrons_reco, tgtpt->pid()));
-    histograms["RecoW"]->Fill(GetW(fslep_reco,beampt));
-    histograms["RecoQ2"]->Fill(GetQ2(fslep_reco,beampt));
-    //    histograms["HadDeltaPT"]
+      // Store histogram with same binning as data. For now is only MC comparison 
+      histograms["Efl"]->Fill(El);
+      histograms["pfl"]->Fill(Pl);
+      histograms["pfl_theta"]->Fill(fslep_reco->momentum().theta()*180/TMath::Pi());
+      histograms["proton_mom"]->Fill(proton->momentum().p3mod());
+      histograms["proton_theta"]->Fill(proton->momentum().theta()*180/TMath::Pi());
+      histograms["pim_mom"]->Fill(pion->momentum().p3mod());
+      histograms["pim_theta"]->Fill(pion->momentum().theta()*180/TMath::Pi());
+      histograms["ECal"]->Fill(GetECal( fslep_reco, hadrons_reco, tgtpt->pid()));
+      histograms["RecoW"]->Fill(GetW(fslep_reco,beampt));
+      histograms["RecoQ2"]->Fill(GetQ2(fslep_reco,beampt));
+      //    histograms["HadDeltaPT"]
    
-    //double evw = FATXAcc->process(evt);
-}
+      //double evw = FATXAcc->process(evt);
+    }
+  }
   std::cout << " Total processed: " << nprocessed << std::endl;
   // Write the histogram to the file
   for ( auto it = histograms.begin(); it != histograms.end(); it++) (it->second)->Write();
